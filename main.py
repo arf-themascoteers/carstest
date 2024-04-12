8286054dabb84d5d55f0a39dfcebbe792661f640
arif <arif@themascoteers.com>
2024-04-12 16:32:21 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from auswahl import CARS
+
+np.random.seed(1337)
+X = np.random.randn(10, 10)
+y = 5 * X[:, 0] - 2 * X[:, 5]
+
+cars = CARS(n_features_to_select=2)
+cars.fit(X, y)
+
+colors = np.full(X.shape[1], fill_value='C00')
+colors[cars.get_support()] = 'C01'
+
+plt.bar(x=np.arange(X.shape[1]), height=cars.feature_importance_, color=colors)
+
+plt.xlabel('Feature')
+plt.ylabel('CARS')
+
+plt.show()
